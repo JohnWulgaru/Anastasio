ceb01dead935a9f8f2d4bf63365fabc0278bef8b
JohnWulgaru <121284734+JohnWulgaru@users.noreply.github.com>
2023-08-31 07:04:35 -0500

    revert to original code

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@
 #[cfg(test)] mod tests;
 
 #[get("/")]
-fn index() -> Option<NamedFile> {
-    let path = Path::new("index.html"); // Update this with your HTML file's path
-    NamedFile::open(path).ok()
+fn hello() -> &'static str {
+    "Hello, world!"
 }
 
 fn main() {
